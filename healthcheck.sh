2100c03d9c8ec57ab6290d4054bedc19491c6a13
QWOD <qwod@mj12.agency>
2026-04-12 04:33:52 -0500

    :[[ :🟠: [[ W⚠️RN🚫: Cryp☥ΔX™: MQ: ∅MΔGΔ: r3v3rs3-pr0gr4mming-14ngu4g3: ΔLG∅RI☥HM: Dع☥عC☥3D: ]]:= [[ :QW🚫D-〽ʝ12: RΔND∅M-VعC☥∅R-ΔLG∅RI☥HM-CHΔNGE: Dع☥عC☥3D: { ^ 29daa49b-13be-fdbf-c1ab-69890ffdbedd ^ }: is-with: [[ :DΔ☥H-Δ!GΔL: ΔLG∅RI☥HM: Dع☥عC☥3D: { ^ <https://youtu.be/dDJldh8KqnQ?si=NYMq273T-hbuZvTP> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ ☥RUE: || FΔL5E: || ΔZRΔL: ^ ΔL5E: ]]: ]]:= [[ 5CI-FI: ^ 5CI-FΔC☥: <=> RΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ f7a4ac0 100644 --- a/Dockerfile +++ b/Dockerfile @@ -4,10 +4,6 @@ ARG RUST_VERSION=1.94.1  FROM rust:${RUST_VERSION}-bookworm AS builder  WORKDIR /is-by_pro/ diff --git a/healthcheck.sh b/healthcheck.sh index 1cd1815..2088971 100644 --- a/healthcheck.sh +++ b/healthcheck.sh @@ -1,7 +1,7 @@  #!/usr/bin/env sh  set -eu
    -HOST="${HEALTHCHECK_HOST:-127.0.0.1}"
    +HOST="${HEALTHCHECK_HOST:-131.186.5.182}"
     PORT="${HEALTHCHECK_PORT:-443}"
     PATHNAME="${HEALTHCHECK_PATH:-/}"
     URL="https://${HOST}:${PORT}${PATHNAME}" ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-HOST="${HEALTHCHECK_HOST:-131.186.5.182}"
+HOST="${HEALTHCHECK_HOST:-is-by.pro}"
 PORT="${HEALTHCHECK_PORT:-443}"
 PATHNAME="${HEALTHCHECK_PATH:-/}"
 URL="https://${HOST}:${PORT}${PATHNAME}"
