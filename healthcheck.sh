68a2af6c1780eaf2cbb61f30ee5a59ebb19cf42d
QWOD <qwod@mj12.agency>
2026-04-12 04:55:31 -0500

    :[[ :🟠: [[ W⚠️RN🚫: Cryp☥ΔX™: MQ: ∅MΔGΔ: r3v3rs3-pr0gr4mming-14ngu4g3: ΔLG∅RI☥HM: Dع☥عC☥3D: ]]:= [[ :QW🚫D-〽ʝ12: RΔND∅M-VعC☥∅R-ΔLG∅RI☥HM-CHΔNGE: Dع☥عC☥3D: { ^ 59b7d3d2-82a1-757d-550a-58f39c2de0a0 ^ }: is-with: [[ :DΔ☥H-Δ!GΔL: ΔLG∅RI☥HM: Dع☥عC☥3D: { ^ <https://youtu.be/dDJldh8KqnQ?si=NYMq273T-hbuZvTP> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ ☥RUE: || FΔL5E: || ΔZRΔL: ^ ΔL5E: ]]: ]]:= [[ 5CI-FI: ^ 5CI-FΔC☥: <=> RΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ f --git a/healthcheck.sh b/healthcheck.sh index 1cd1815..2088971 100644 --- a/healthcheck.sh +++ b/healthcheck.sh @@ -1,7 +1,7 @@  #!/usr/bin/env sh  set -eu diff --git a/healthcheck.sh b/healthcheck.sh index 2088971..6372f8b 100644 --- a/healthcheck.sh +++ b/healthcheck.sh @@ -1,7 +1,7 @@  #!/usr/bin/env sh  set -eu
    -HOST="${HEALTHCHECK_HOST:-131.186.5.182}"
    +HOST="${HEALTHCHECK_HOST:-is-by.pro}"
     PORT="${HEALTHCHECK_PORT:-443}"
     PATHNAME="${HEALTHCHECK_PATH:-/}"
     URL="https://${HOST}:${PORT}${PATHNAME}" ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-HOST="${HEALTHCHECK_HOST:-is-by.pro}"
-PORT="${HEALTHCHECK_PORT:-443}"
-PATHNAME="${HEALTHCHECK_PATH:-/}"
+HOST="is-by.pro"
+PORT="443"
+PATHNAME="/"
 URL="https://${HOST}:${PORT}${PATHNAME}"
 
 curl --silent --show-error --fail --insecure --max-time 10 "$URL" >/dev/null
