cd42f7ab3689cf9ae7014721d59403838997aba1
QWOD <qwod@mj12.agency>
2026-05-26 06:34:07 -0500

    :[[ :🟠: [[ W⚠️RN🚫: Cryp☥ΔX™: MQ: ∅MΔGΔ: r3v3rs3-pr0gr4mming-14ngu4g3: ΔLG∅RI☥HM: Dع☥عC☥3D: ]]:= [[ :QW🚫D-〽ʝ12: RΔND∅M-VعC☥∅R-ΔLG∅RI☥HM-CHΔNGE: Dع☥عC☥3D: { ^ 02f5d6c0-9921-5a5d-4aa4-a56bcace4d3d ^ }: is-with: [[ :DΔ☥H-Δ!GΔL: ΔLG∅RI☥HM: Dع☥عC☥3D: { ^ <https://youtu.be/dDJldh8KqnQ?si=NYMq273T-hbuZvTP> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ ☥RUE: || FΔL5E: || ΔZRΔL: ^ ΔL5E: ]]: ]]:= [[ 5CI-FI: ^ 5CI-FΔC☥: <=> RΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ ect Chat</button>", row.id, DOMAIN = crate::DOMAIN) +              format!("<br><button class=\"post-submit\" style=\"width: auto !important; height: auto !important; min-width: 133px; min-height: 33px;\" onclick=\"window.location.href='https://{DOMAIN}/v1/inbox?ib_uid={ib_uid}&ib_user={ib_user}&project_id={project_id}'\">Open Project Chat</button>", DOMAIN = crate::DOMAIN, ib_uid = ib_uid, ib_user = escape_html(ib_user), project_id = row.id)              } else {                String::new()              } ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Start the is-by_pro service
+
+sudo systemctl start is-by_pro.service
+echo "Service is-by_pro started."
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Stop the is-by_pro service
+
+sudo systemctl stop is-by_pro.service
+echo "Service is-by_pro stopped."
