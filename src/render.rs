334f5efe10a616ac2efba393bbcc408d7ddcab18
QWOD <qwod@mj12.agency>
2026-05-27 06:09:01 -0500

    :[[ :🟠: [[ W⚠️RN🚫: Cryp☥ΔX™: MQ: ∅MΔGΔ: r3v3rs3-pr0gr4mming-14ngu4g3: ΔLG∅RI☥HM: Dع☥عC☥3D: ]]:= [[ :QW🚫D-〽ʝ12: RΔND∅M-VعC☥∅R-ΔLG∅RI☥HM-CHΔNGE: Dع☥عC☥3D: { ^ e9ca6ede-0513-80a5-fba7-da52fd7259c9 ^ }: is-with: [[ :DΔ☥H-Δ!GΔL: ΔLG∅RI☥HM: Dع☥عC☥3D: { ^ <https://youtu.be/dDJldh8KqnQ?si=NYMq273T-hbuZvTP> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ ☥RUE: || FΔL5E: || ΔZRΔL: ^ ΔL5E: ]]: ]]:= [[ 5CI-FI: ^ 5CI-FΔC☥: <=> RΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ d4a0f19 100644 --- a/src/render.rs +++ b/src/render.rs @@ -5419,6 +5419,11 @@ fn extract_youtube_video_id(url: &str) -> Option<String> {      let rest = &url[start..];      let end = rest.find('?').unwrap_or(rest.len());      Some(rest[..end].to_string()) +  } else if let Some(pos) = url.find("youtube.com/live/") { +    let start = pos + "youtube.com/live/".len(); +    let rest = &url[start..]; +    let end = rest.find('?').unwrap_or(rest.len()); +    Some(rest[..end].to_string())    } else {      None    } ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- src/render.rs
+++ src/render.rs
@@ -509,7 +509,7 @@ pub async fn render_profile_html(
     let display_rows = &ib_post_results[..ib_post_results.len().min(20)];
 
     let mut selected_user_posts_response_content = format!(
-      r#"<br><div class="notice"><p><em>:[[ :for-the: [[ posts: is-by: {ib_post_results_length}: is-with: showing-latest-results: ]]:</em></p></div>"#,
+      r#"<br><div class="notice"><p><em>:[[ :for-the: posts: is-by: {ib_post_results_length}: is-with: showing-latest-results: ]]:</em></p></div>"#,
       ib_post_results_length = ib_post_results_length,
     );
 
@@ -834,7 +834,7 @@ pub async fn render_profile_mobile_html(
     )
   } else {
     format!(
-      r#"<br><div class="notice"><p><em>:[[ :for-the: [[ posts: is-by: {ib_post_results_length}: is-with: showing-latest-results: ]]:</em></p></div>"#,
+      r#"<br><div class="notice"><p><em>:[[ :for-the: posts: is-by: {ib_post_results_length}: is-with: showing-latest-results: ]]:</em></p></div>"#,
       ib_post_results_length = ib_post_results_length,
     )
   };
